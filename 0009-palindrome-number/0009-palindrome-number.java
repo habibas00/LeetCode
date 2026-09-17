deb82622d4082f4668272d1146e47ee152bc426f
Habib Asanur Mondal <habibasanur2004@gmail.com>
2026-09-17 15:21:00 +0530

    Time: 5 ms (83.07%), Space: 46 MB (52.68%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        int num = x;
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+
+            int digit = num % 10;
+
+            reverse = reverse * 10 + digit;
+
+            num = num / 10;
+        }
+
+        if (original == reverse) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
